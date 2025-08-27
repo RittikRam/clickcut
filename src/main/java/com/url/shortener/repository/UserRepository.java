2fc1b351d3f52736ec59f577b7136415bba2d530
Rittik Ram <hrittickramspi40@gmail.com>
2025-08-27 20:44:47 +0530

    commit -m "Implemented backend logic"

---
+++ src/main/java/com/url/shortener/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.url.shortener.repository;
+
+import com.url.shortener.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
---
+++ src/main/java/com/url/shortener/service/UrlMappingService.java
@@ -0,0 +1,56 @@
+package com.url.shortener.service;
+
+import com.url.shortener.dtos.UrlMappingDto;
+import com.url.shortener.model.UrlMapping;
+import com.url.shortener.model.User;
+import com.url.shortener.repository.UrlMappingRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class UrlMappingService {
+    private UrlMappingRepository urlMappingRepository;
+
+    public UrlMappingDto createShortUrl(String originalUrl, User user){
+        String shortUrl = generateShortUrl(originalUrl);
+        UrlMapping urlMapping = new UrlMapping();
+        urlMapping.setOriginalUrl(originalUrl);
+        urlMapping.setUser(user);
+        urlMapping.setCreatedDate(LocalDateTime.now());
+        urlMapping.setShortUrl(shortUrl);
+        UrlMapping savedUrlMapping = urlMappingRepository.save(urlMapping);
+        return convertToDto(savedUrlMapping);
+    }
+    private UrlMappingDto convertToDto(UrlMapping urlMapping){
+        UrlMappingDto urlMappingDto = new UrlMappingDto();
+        urlMappingDto.setId(urlMapping.getId());
+        urlMappingDto.setOriginalUrl(urlMapping.getOriginalUrl());
+        urlMappingDto.setShortUrl(urlMapping.getShortUrl());
+        urlMappingDto.setCreatedDate(urlMapping.getCreatedDate());;
+        urlMappingDto.setUsername(urlMapping.getUser().getUsername());
+        urlMappingDto.setClickCount(urlMapping.getClickCount());
+        return urlMappingDto;
+
+    }
+
+    private String generateShortUrl(String originalUrl) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder shortUrl = new StringBuilder(8);
+        for(int i=0; i<8; i++){
+            shortUrl.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return shortUrl.toString();
+    }
+
+    public List<UrlMappingDto> getUrlsByUser(User user) {
+        return urlMappingRepository.findByUser(user).stream()
+                .map(this::convertToDto)
+                .toList();
+    }
+}
